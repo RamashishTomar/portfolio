388b8a9df246c3dd571d9275f9f162275c594741
RamashishTomar <ramashishtomarofc@gmail.com>
2019-08-28 15:33:52 +0530

    Updates

---
+++ precache-manifest.fa6c83eb28f5014eb4365bf4e3068d80.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "466340759e827edfccc3ca10c9b48ce0",
+    "url": "/RamashishTomar/portfolio/index.html"
+  },
+  {
+    "revision": "4edbee71a4ecdc3a541c",
+    "url": "/RamashishTomar/portfolio/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "21d374903c7e5529b265",
+    "url": "/RamashishTomar/portfolio/static/js/2.4b1ba94f.chunk.js"
+  },
+  {
+    "revision": "4edbee71a4ecdc3a541c",
+    "url": "/RamashishTomar/portfolio/static/js/main.c3c5793f.chunk.js"
+  },
+  {
+    "revision": "f7c5d88fee00fdccf12b",
+    "url": "/RamashishTomar/portfolio/static/js/runtime~main.a4c73bbb.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/RamashishTomar/portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
